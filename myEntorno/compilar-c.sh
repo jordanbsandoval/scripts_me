d2ecf6f0c921cd273956003c13179c0b782dc531
jordanbsandoval <1608@holbertonschool.com>
2021-09-10 19:10:26 -0500

    organizando

--- myEntorno/compilar-c.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gcc -Wall -Wextra -Werror -pedantic -std=gnu89 $1 -o $(echo $1 | cut -d "." -f "1")
--- myEntorno/docker-compose.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Descargando docker --version 20.10.5 y envio el ejecutable a los binarios del usuario
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-# doy permisos de ejecucion
-sudo chmod +x /usr/local/bin/docker-compose
